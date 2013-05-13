2939d64a5e52c15ad9d356f22899a1494a6d4623
Heiher <admin@heiher.info>
2013-05-13 13:12:07 +0800

    Update for linux-3.9.x.

--- panasonic-hbtn.c
+++ panasonic-hbtn.c
@@ -48,7 +48,7 @@ MODULE_LICENSE("GPL");
 #define ACPI_PCC_INPUT_PHYS    "panasonic/hbtn0"
 
 static int acpi_pcc_hbtn_add(struct acpi_device *device);
-static int acpi_pcc_hbtn_remove(struct acpi_device *device, int type);
+static int acpi_pcc_hbtn_remove(struct acpi_device *device);
 static void acpi_pcc_hbtn_notify(struct acpi_device *device, u32 event);
 
 static const struct acpi_device_id pcc_device_ids[] = {
@@ -236,7 +236,7 @@ static int __init acpi_pcc_init(void)
     return 0;
 }
 
-static int acpi_pcc_hbtn_remove(struct acpi_device *device, int type)
+static int acpi_pcc_hbtn_remove(struct acpi_device *device)
 {
     struct pcc_acpi *pcc = acpi_driver_data(device);
 
